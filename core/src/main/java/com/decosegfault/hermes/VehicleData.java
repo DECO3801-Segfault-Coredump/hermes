1c492f893d2cd71a001c51ebd32ac7e013a0efdd
Lachlan Ellis <37792607+lellis0406@users.noreply.github.com>
2023-08-23 23:51:50 +1000

    Vehicle nodemap basis and hermes main class

---
+++ core/src/main/java/com/decosegfault/hermes/VehicleData.java
@@ -0,0 +1,9 @@
+package com.decosegfault.hermes;
+
+import com.badlogic.gdx.math.Vector3;
+
+public class VehicleData {
+    int routeNumber;
+    int vehicleType;
+    Vector3 position; //describes the 2d position and the orientation
+}
