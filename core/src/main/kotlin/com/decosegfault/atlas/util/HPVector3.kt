75a53190faa78a3d75310fbd65a06ce860ff8645
Matt Young <matt.young.1@outlook.com>
2023-09-27 01:17:11 +1000

    Added HPVector3

---
+++ core/src/main/kotlin/com/decosegfault/atlas/util/HPVector3.kt
@@ -0,0 +1,49 @@
+package com.decosegfault.atlas.util
+
+import com.badlogic.gdx.math.MathUtils
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+/**
+ * High precision version of Vector3 intended for WGS84 lat long coordinates
+ * @author Matt Young
+ */
+data class HPVector3(
+    var x: Double,
+    var y: Double,
+    var z: Double,
+) {
+    fun add(v: HPVector3): HPVector3 {
+        x += v.x
+        y += v.y
+        z += v.z
+        return this
+    }
+
+    fun sub(v: HPVector3): HPVector3 {
+        x -= v.x
+        y -= v.y
+        z -= v.z
+        return this
+    }
+
+    fun dst(other: HPVector3): Double {
+        val x_d = other.x - x
+        val y_d = other.y - y
+        return sqrt((x_d * x_d + y_d * y_d))
+    }
+
+    fun crs(v: HPVector3): Double {
+        return x * v.y - y * v.x
+    }
+
+    fun dot(v: HPVector3): Double {
+        return x * v.x + y * v.y
+    }
+
+    fun angleDeg(reference: HPVector3): Double {
+        var angle = atan2(reference.crs(this), reference.dot(this)) * MathUtils.radiansToDegrees
+        if (angle < 0) angle += 360f
+        return angle
+    }
+}
