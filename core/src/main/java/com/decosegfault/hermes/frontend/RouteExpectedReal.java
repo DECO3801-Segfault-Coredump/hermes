c3116cd6eff0cb77313943adc7713a4647c5775c
cath <49269828+cathyngyn@users.noreply.github.com>
2023-10-19 15:05:40 +1000

    stufufuf ufufuf ufafjds

---
+++ core/src/main/java/com/decosegfault/hermes/frontend/RouteExpectedReal.java
@@ -0,0 +1,32 @@
+package com.decosegfault.hermes.frontend;
+
+public class RouteExpectedReal {
+
+    String routeName;
+    double expectedTime;
+    double actualTime;
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+    }
+
+    public double getExpectedTime() {
+        return expectedTime;
+    }
+
+    public void setExpectedTime(double expectedTime) {
+        this.expectedTime = expectedTime;
+    }
+
+    public double getActualTime() {
+        return actualTime;
+    }
+
+    public void setActualTime(double actualTime) {
+        this.actualTime = actualTime;
+    }
+}
